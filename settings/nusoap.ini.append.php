26d6112fc614c66b4b8fdde0acf7887ef1c2f34a
Brookins Consulting <info@brookinsconsulting.com>
2007-08-10 10:14:29 +0000

    Added: settings examples

---
+++ settings/nusoap.ini.append.php
@@ -0,0 +1,17 @@
+<? /*
+
+# NuSOAP services configuration file
+
+[GeneralSettings]
+AvailableServices[]=bcsoapsearch
+
+# example service bcsoapsearch
+[Service_bcsoapsearch]
+# name of the service
+ServiceName=BcSoapSearch
+# namespace of the service
+ServiceNamespace=urn:bcsoapsearch
+
+SOAPExtensions[nusoap]=bcsoapsearch
+
+*/ ?>
