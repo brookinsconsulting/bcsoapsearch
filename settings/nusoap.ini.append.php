1b65578567c640b4e94c7b9feb2dd9acd7cef8fa
Brookins Consulting <info@brookinsconsulting.com>
2007-09-27 09:39:16 +0000

    Updated: nusoap service auto detection settings

--- settings/nusoap.ini.append.php
+++ settings/nusoap.ini.append.php
@@ -8,17 +8,18 @@ AvailableServices[]=bcsoapsearch
 # enable or disable NuSOAP server
 EnableSOAP=true
 
-# example service bcsoapsearch                                                                                                          
+# enable or disable NuSOAP server logging
+EnableLog=true
+
+# example service bcsoapsearch
 [Service_bcsoapsearch]
 # name of the service
 ServiceName=BcSoapSearch
-
 # namespace of the service
 ServiceNamespace=urn:bcsoapsearch
-
 # extensions which contain the methods
 # the eZ publish extension nusoap contains a NuSOAP extension called helloworld
 # SOAPExtensions[]
-SOAPExtensions[nusoap]=bcsoapsearch
+SOAPExtensions[bcsoapsearch]=bcsoapsearch
 
 */ ?>
